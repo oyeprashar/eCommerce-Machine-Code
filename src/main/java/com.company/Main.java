802dddee74fb98f53005fe2fceb54c012813fef2
Shubham Prashar <67927586+oyeprashar@users.noreply.github.com>
2022-01-10 18:28:38 +0530

    Add files via upload

---
+++ src/main/java/com.company/Main.java
@@ -0,0 +1,8 @@
+package main.java.com.company;
+import main.java.com.company.services.ECommerce;
+
+public class Main {
+    public static void main(String[] args) {
+        ECommerce.startApp();
+    }
+}
